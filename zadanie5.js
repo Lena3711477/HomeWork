9d9bd02ed30ac0fcf1f7c2ada29699b9d3627f36
Lena3711477 <elena.polyakova208@outlook.com>
2023-10-09 22:29:55 +0300

    add file zadanie5

---
+++ zadanie5.js
@@ -0,0 +1,32 @@
+//sum string+boolran
+let str1='Hello';
+let bol=true;
+console.log('string=',str1,' + boolean=',bol,'summa= ',sum=str1+bol)
+// sum string+number
+let str2='31';
+let num1=18 
+console.log('string=',str2,' + number=',num1,'summa= ',sum=str2+num1);
+// sum number + boolean
+console.log('boolean=',bol,' + number=',num1,'summa= ',sum=bol+num1);
+// multiplication (string * boolean)
+console.log('string=',str1,' * boolean=',bol,'summa= ',mul=str1*bol);
+// multiplication (string * number)
+console.log('string=',str2,' * number=',num1,'summa= ',sum=str2*num1);
+// multiplication (number * boolean)
+console.log('boolean=',bol,' * number=',num1,'summa= ',sum=bol*num1);
+// division (string / boolean)
+console.log('string=',str1,' / boolean=',bol,'summa= ',mul=str1/bol);
+// division (string / number)
+console.log('string=',str2,' / number=',num1,'summa= ',sum=str2/num1);
+// division (number / boolean)
+console.log('boolean=',bol,' / number=',num1,'summa= ',sum=bol/num1);
+//явное преобразование:
+const x = '4';
+Number(x);  // строки в число
+console.log(x*x);
+const y = 4;  // числа в строку
+String(y);
+console.log(y+y);
+console.log(Boolean('IT')); // перобразоваение строки в boolean
+console.log(Boolean(5)); // перобразоваение числа в boolean
+console.log(Boolean()); // перобразоваение в boolean
